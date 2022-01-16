020f465a13682c51295aebc889e53c63db86d717
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-01-16 15:11:17 -0300

    validates no model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+require 'valid_email'
+
 class User < ApplicationRecord
   has_secure_password
+  
+  include ActiveModel::Validations
+  attr_writer  :email
+
+  validates :name, presence: true, length: { maximum: 50 } 
+  validates :password, presence: true, length: { maximum: 6 }
+  validates :email, presence: true, email: true
 end
